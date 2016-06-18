c8a9a63b4c6a75838180ee1334822714c18b1c19
Roland <rskalmogo@gmail.com>
2016-06-17 22:04:14 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// event listener to respond to clicks on the page
+// when user clicks anywhere on the page, the "makeQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
