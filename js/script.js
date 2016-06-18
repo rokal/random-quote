03e4d11045d5869ae0e109fd5d9e61b3bd07feb9
Roland <rskalmogo@gmail.com>
2016-06-18 04:39:36 -0400

    extra credit 3

--- js/script.js
+++ js/script.js
@@ -37,19 +37,30 @@ var quotes=[
     }
 ];
 
-var getRandomQuote = function (){
+/* will be used to all quotes before display any quote for the second time */
+var copyQuotes=[]
+
+function getRandomQuote(quotes){
     /*
          shoose random number between 0 and quotes.length-1
     */
     var randomNumber=Math.floor(Math.random()*(quotes.length-1));
-
+    console.log('randomNumber : ' + randomNumber);
     return quotes[randomNumber];
 }
 
-var printQuote = function (){
-    var randomQuote = getRandomQuote(),
+function printQuote(){
+    /* if the copyQuotes is empty, then we copy the content of the initial array of quotes in it */
+    if(copyQuotes.length==0){
+        copyQuotes=customCopy(quotes);
+    }
+    /* select a randomQuote in the copyQuotes and delete it from the copyQuotes so 
+        that next time, we'll shoose different quote 
+    */
+    var randomQuote = getRandomQuote(copyQuotes),
         htmlQuote = '';
-    
+    var indexOfRandomQuote=copyQuotes.indexOf(randomQuote);
+    copyQuotes.splice(indexOfRandomQuote,1);
     htmlQuote += '<p class="quote"> ' + randomQuote.quote +' </p>'+
                 '<p class="source">' + randomQuote.source;
     if(randomQuote.citation){
@@ -60,6 +71,21 @@ var printQuote = function (){
     }
     htmlQuote += '</p>';
 
-    document.getElementById('quote-box').innerHTML(htmlQuote);
+    document.getElementById('quote-box').innerHTML = htmlQuote;
 }
+/*  function  to copy an object */
+function customCopy(obj) {
+    if(null==obj || "object"!= typeof obj){
+        return obj;
+    }else{
+        var copy=obj.constructor();
+        for(var attribute in obj){
+            copy[attribute]=obj[attribute];
+        }
+        return copy;
+
+    }
+}
+
+
 
